bb62ba45e209cb6b9075ff8fbdfaab1bee9411aa
D7703_13 <d7703_13@D7703-13ui-iMac.local>
2017-09-12 10:35:20 +0900

    add print

--- 01Git_Test_Branch_T/ViewController.swift
+++ 01Git_Test_Branch_T/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("1st commit")
         print("bye byt")
+        print("second commit")
     }
 
     override func didReceiveMemoryWarning() {
